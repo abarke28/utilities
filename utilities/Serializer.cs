21e52d3548b3077e5716fce6c9ec3d342a32ed27
Alex Barker <abarke28@gmail.com>
2020-03-21 07:47:31 -0700

    Add project files.

---
+++ utilities/Serializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace utilities
+{
+    public static class Serializer
+    {
+        public static void SerializeToXmlFile<T>(string filePath, object file)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+
+            using XmlWriter xmlWriter = XmlWriter.Create(filePath);
+            xmlSerializer.Serialize(xmlWriter, file);
+        }
+
+        public static T DeserializeFromXmlFile<T>(string filePath)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+
+            using XmlReader xmlReader = XmlReader.Create(filePath);
+            return (T)xmlSerializer.Deserialize(xmlReader);
+        }
+    }
+}
