2b9c76e8d7504fa54eb6f0a37acb123b2cdabf3e
Alex Barker <abarke28@gmail.com>
2020-03-22 21:40:10 -0700

    Added additional logic to IsValidFilePathAndName method

--- utilities/FileUtility.cs
+++ utilities/FileUtility.cs
@@ -7,17 +7,28 @@ namespace utilities
 {
     public static class FileUtility
     {
-        public static bool IsValidFilePath(string filePath)
+        public static bool IsValidFilePathAndName(string filePath)
         {
             // Summary
             //
             // Validates supplied path for a file does not contain any invalid characters
 
+            // Validate no invalid characters are in path
             string invalidCharacters = new string(Path.GetInvalidPathChars()) + new string(Path.GetInvalidFileNameChars());
 
             foreach (char c in invalidCharacters)
             {
                 if (filePath.Contains(c)) return false;
+            }
+
+            // Use FileInfo to validate File could be made
+            try
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
+            }
+            catch (Exception)
+            {
+                return false;                
             }
 
             return true;
