fa9de03c8f62cf51b712d518a73c25a8224aab3f
Alex Barker <abarke28@gmail.com>
2020-04-06 15:33:16 -0700

    Added BaseVM & BaseCommand classes

---
+++ utilities/BaseCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace utilities
+{
+    class BaseCommand : ICommand
+    {
+        private readonly Predicate<object> _canExecuteMethod = null;
+        private readonly Action<object> _executeMethod = null;
+
+        public BaseCommand(Predicate<object> canExecuteMethod, Action<Object> executeMethod)
+        {
+            _canExecuteMethod = canExecuteMethod ?? throw new ArgumentNullException(nameof(canExecuteMethod));
+            _executeMethod = executeMethod ?? throw new ArgumentNullException(nameof(executeMethod));
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteMethod.Invoke(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeMethod.Invoke(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ utilities/BaseVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace utilities
+{
+    public class BaseVM : INotifyPropertyChanged
+    {
+        public void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
