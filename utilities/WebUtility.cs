ad5d26813e110272e30e132f99e31701ff33a9aa
Alex Barker <abarke28@gmail.com>
2020-05-19 08:51:01 -0700

    meta data for intellisense

--- utilities/WebUtility.cs
+++ utilities/WebUtility.cs
@@ -54,31 +54,29 @@ namespace utilities
             return true;
         }
 
+        /// <summary>
+        /// Extension: Send a POST request to the specified Uri as an asynchronous operation.
+        /// Content is serialized as Json with UTF8 encoding.
+        /// </summary>
+        /// <param name="requestUri">The Uri the request is sent to.</param>
         public static Task<HttpResponseMessage> PostAsync(this HttpClient http, string requestUri, object content)
         {
-            /// <summary>
-            /// Extension: Send a POST request to the specified Uri as an asynchronous operation.
-            /// Content is serialized as Json with UTF8 encoding.
-            /// </summary>
-            /// <param name="requestUri">The Uri the request is sent to.</param>
-
             var objectJson = JsonConvert.SerializeObject(content);
             var contentStr = new StringContent(objectJson, Encoding.UTF8, "application/json");
 
             return http.PostAsync(requestUri, contentStr);
         }
 
+        /// <summary>
+        /// Extension: Send a PUT request to teh specified Uri as an asynchronous operation.
+        /// Content is serialized as Json with UTF8 encoding.
+        /// </summary>
+        /// <param name="http"></param>
+        /// <param name="requestUri"></param>
+        /// <param name="content"></param>
+        /// <returns></returns>
         public static Task<HttpResponseMessage> PutAsync(this HttpClient http, string requestUri, object content)
         {
-            /// <summary>
-            /// Extension: Send a PUT request to teh specified Uri as an asynchronous operation.
-            /// Content is serialized as Json with UTF8 encoding.
-            /// </summary>
-            /// <param name="http"></param>
-            /// <param name="requestUri"></param>
-            /// <param name="content"></param>
-            /// <returns></returns>
-
             var objectJson = JsonConvert.SerializeObject(content);
             var contentStr = new StringContent(objectJson, Encoding.UTF8, "application/json");
 
